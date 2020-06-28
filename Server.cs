250f5a424a3656fe4801b3516286cf268886740d
backskin <backskin@outlook.com>
2020-06-28 19:36:14 +0400

    basic test of a project

--- Server.cs
+++ Server.cs
@@ -1,7 +1,26 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using System.Diagnostics;
+using vidak_backend;
+
+public class Server
 {
-    public class Server
+
+    public static void Write(string s)
     {
-        
+        Console.WriteLine(s);
+    }
+
+    public static void Write(bool b)
+    {
+        Console.WriteLine(b);
+          
+    }
+
+    static void Main()
+    {
+        string name = "Scott";
+        System.Diagnostics.Debug.WriteLine($"Hello, {name}");
+        var uc = new UniversalClass();
+        uc.DoMath();
     }
 }
---
+++ UniversalClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace vidak_backend
+{
+    class UniversalClass
+    {
+
+        private void check(decimal a, decimal b)
+        {
+            Console.Write($"{a} + {b} > 10? ");
+            bool something = a + b > 10m;
+            if (something)
+            {
+                Console.Write("It's true");
+            }
+            else
+            {
+                Console.Write("It's false");
+            }
+        }
+
+        public void DoMath()
+        {
+            decimal a = 5;
+            decimal b = 6;
+            check(a, b);
+            b = 3;
+            check(a, b);
+            var line = Console.ReadLine();
+        }
+    }
+}
