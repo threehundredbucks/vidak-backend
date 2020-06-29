0c8d7f253885bab5e6d633b74f6ee4c494e10f43
backskin <backskin@outlook.com>
2020-06-29 13:28:54 +0400

    ++++

--- UniversalClass.cs
+++ UniversalClass.cs
@@ -5,19 +5,30 @@ namespace vidak_backend
     class UniversalClass
     {
 
-        private void check(decimal a, decimal b)
+        private void Check(decimal a, decimal b)
         {
             var something = a + b > 10m;
             Console.WriteLine($"{a} + {b} > 10? " + (something ? "It's true" : "It's false"));
+        }
+
+        private void Japanese()
+        {
+            // To generate Japanese invoices:
+            // Nihon no seikyū-sho o seisei suru ni wa:
+            Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+            Console.WriteLine(@"c:\invoices\app.exe -j");
         }
 
         public void DoMath()
         {
             decimal a = 5;
             decimal b = 6;
-            check(a, b);
+            Check(a, b);
             b = 3;
-            check(a, b);
+            Check(a, b);
+            Japanese();
+            Random dice = new Random();
+            Console.WriteLine($"Random number = {dice.Next(1, 100)} (from 1 to 99)");
         }
     }
 }
